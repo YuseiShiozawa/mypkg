99a73048459462382a281f8e845d7f93cfe29748
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-08 19:44:04 +0900

    talker change

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,16 +1,17 @@
-import rclpy                     #ROS2のクライアントのためのライブラリ
-from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
+import rclpy                     
+from rclpy.node import Node      
+from person_msgs.msg import Person   
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
+pub = node.create_publisher(Person, "person", 10)
 n = 0
 
 def cb():
     global n
-    msg = Int16()
-    msg.data = n
+    msg = Person()
+    msg.name = "YUSEISHIOZAWA"
+    msg.age = n
     pub.publish(msg)
     n += 1
 
