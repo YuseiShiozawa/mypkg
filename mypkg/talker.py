2f2cf2cdfdb2b46771556cde674f2506700ede1f
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-14 21:39:45 +0900

    make a method

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,19 +3,18 @@ from rclpy.node import Node
 from std_msgs.msg import Int16   
 
 class Talker():
-    def __init__(self):
-        self.pub = node.create_publisher(Int16, "countup", 10)
+    def __init__(self, nh):
+        self.pub = nh.create_publisher(Int16, "countup", 10)
         self.n = 0
+        nh.create_timer(0.5, self.cb)
+
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 rclpy.init()
 node = Node("talker")
-talker = Talker()
-
-def cb():
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
-
-node.create_timer(0.5, cb)
+talker = Talker(node)
 rclpy.spin(node)
