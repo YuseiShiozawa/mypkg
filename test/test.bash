76862621f9510c0589d543d5561f42f9b99e2e22
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-25 04:32:40 +0900

    new test ok?

--- test/test.bash
+++ test/test.bash
@@ -9,9 +9,6 @@ colcon build
 source $dir/.bashrc
 
 cd src/mypkg/test/
-echo "read" > "option.txt"
-ls option.txt
-#timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 #ros2 run mypkg sudoku_ans < 'sudoku.txt' & 
 ros2 run mypkg sudoku_problem &
 ros2 run mypkg sudoku_ans < 'sudoku.txt' > /tmp/mypkg.log
@@ -21,5 +18,4 @@ ros2 run mypkg sudoku_ans < 'sudoku.txt' > /tmp/mypkg.log
 cat /tmp/mypkg.log | grep "...."
 #cat /tmp/mypkg.log
 #cat /tmp/mypkg.log | grep "0"
-cat /tmp/mypkg.log | grep '0'
 
