8d664542ed2c39934ef7280afcb72462b54877fe
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-24 21:54:40 +0900

    ok test?

--- test/test.bash
+++ test/test.bash
@@ -8,9 +8,13 @@ colcon build
 
 source $dir/.bashrc
 
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-
+cd src/mypkg/test/
+echo "read" > "option.txt"
+ls option.txt
+#timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+ros2 run mypkg sudoku_ans < 'sudoku.txt' &
+ros2 run mypkg sudoku_problem > /tmp/mypkg.log
+#cat /tmp/mypkg.log |
+#grep 'Listen: 10'
 cat /tmp/mypkg.log |
-grep 'Listen: 10'
-
-
+grep '....'
