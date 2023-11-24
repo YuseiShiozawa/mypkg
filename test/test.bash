f1c35333b4f1be937c8474cccabc9d1073ae6d18
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-25 04:01:15 +0900

    new test ok?

--- test/test.bash
+++ test/test.bash
@@ -13,7 +13,8 @@ cd src/mypkg/test/
 #ls option.txt
 #timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 ros2 run mypkg sudoku_ans < 'sudoku.txt' > /tmp/mypkg.log &
-ros2 run mypkg sudoku_problem > /tmp/mypkg.log
+ros2 run mypkg sudoku_problem 
+#> /tmp/mypkg.log
 #cat /tmp/mypkg.log |
 #grep 'Listen: 10'
 #cat /tmp/mypkg.log | grep "...."
