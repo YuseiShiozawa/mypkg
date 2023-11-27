d93a13945e7236b36cfb36f0a2897f5f251e77ef
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-27 22:07:37 +0900

    error check13

--- test/test.bash
+++ test/test.bash
@@ -11,14 +11,13 @@ source $dir/.bashrc
 cd src/mypkg/test/
 ros2 run mypkg sudoku_ans < 'sudoku.txt' &
 ros2 run mypkg sudoku_problem > /tmp/mypkg.log
+#ros2 run mypkg sudoku_ans < 'sudoku.txt' > /tmp/mypkg.log
 #> /tmp/mypkg.log
 #cat /tmp/mypkg.log |
 #grep 'Listen: 10'
 #cat /tmp/mypkg.log | grep "...."
 #cat /tmp/mypkg.log
 #cd $dir/ros2_ws
-##cat /tmp/mypkg.log | grep "0"
-#cat /tmp/mypkg.log | grep "...." & grep "0"
 cat /tmp/mypkg.log | grep "0"
 #cat /tmp/mypkg.log | grep "no"
 
