6cdfb2b39f08b8430c28d01faca552175af0f5de
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-27 23:14:34 +0900

    error check23

--- test/test.bash
+++ test/test.bash
@@ -8,10 +8,9 @@ colcon build
 
 source $dir/.bashrc
 
-cd src/mypkg/test/
+##cd src/mypkg/test/
 ros2 run mypkg sudoku_ans < 'sudoku.txt' &
-ros2 run mypkg sudoku_problem > log.txt
-#> /tmp/mypkg.log
+ros2 run mypkg sudoku_problem > /tmp/mypkg.log
 #ros2 run mypkg sudoku_ans < 'sudoku.txt' > /tmp/mypkg.log
 #> /tmp/mypkg.log
 #cat /tmp/mypkg.log |
@@ -19,7 +18,6 @@ ros2 run mypkg sudoku_problem > log.txt
 #cat /tmp/mypkg.log | grep "...."
 #cat /tmp/mypkg.log
 #cd $dir/ros2_ws
-cat log.txt | grep "no"
-##cat /tmp/mypkg.log | grep "0"
+cat /tmp/mypkg.log | grep "0"
 #cat /tmp/mypkg.log | grep "no"
 
