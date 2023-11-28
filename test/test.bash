2ea3a58fe629df2dda90228501b8b053a058e8cd
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-28 18:20:55 +0900

    error check16

--- test/test.bash
+++ test/test.bash
@@ -21,7 +21,7 @@ ros2 run mypkg sudoku_problem > /tmp/mypkg.log
 #cat /tmp/mypkg.log
 #cd $dir/ros2_ws
 ##cat /tmp/mypkg.log | grep "0"
-cat /tmp/mypkg.log | grep "0"
+cat /tmp/mypkg.log | grep "...."
 #cat /tmp/mypkg.log | grep "no"
 
 
