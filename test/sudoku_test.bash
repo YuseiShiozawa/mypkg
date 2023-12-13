fc631ec20c5261aa6407ca81aa791a23b3cdd93b
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-12-13 21:24:35 +0900

    modify test

--- test/sudoku_test.bash
+++ test/sudoku_test.bash
@@ -9,11 +9,7 @@ cd $dir/ros2_ws
 colcon build
 
 source $dir/.bashrc
-#cd src/mypkg/test/
-#timeout 25 ros2 run mypkg sudoku_ans &
-#ros2 run mypkg sudoku_problem > /tmp/mypkg.log
 timeout 245 ros2 launch mypkg sudoku_ans_problem.launch.py > /tmp/mypkg.log
-#245
 cat /tmp/mypkg.log | 
 grep "Correct"
 
